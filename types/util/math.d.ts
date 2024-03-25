dec7ac442063e117d639068daaf2c47bf5189dca
Joe Eagar <joeedh@gmail.com>
2024-03-25 15:36:24 -0700

    Update types

--- types/util/math.d.ts
+++ types/util/math.d.ts
@@ -1,48 +1,48 @@
-import { Vector3, BaseVector, Vector2, Matrix4 } from './vectormath'
+import { Vector3, BaseVector, Vector2, Matrix4 } from "./vectormath";
 
 export declare function colinear(
-    a: BaseVector,
-    b: BaseVector,
-    c: BaseVector,
-    limit?: number,
-    distLimit?: number
-): boolean
+  a: BaseVector,
+  b: BaseVector,
+  c: BaseVector,
+  limit?: number,
+  distLimit?: number
+): boolean;
 
-export declare function aabb_isect_line_2d(a: Vector2, b: Vector2, min: Vector2, max: Vector2): boolean
+export declare function aabb_isect_line_2d(a: Vector2, b: Vector2, min: Vector2, max: Vector2): boolean;
 
-export declare function colinear2d(a: Vector2, b: Vector2, c: Vector2, limit?: number, distLimit?: number): boolean
+export declare function colinear2d(a: Vector2, b: Vector2, c: Vector2, limit?: number, distLimit?: number): boolean;
 
-export declare function winding(a: BaseVector, b: BaseVector, c: BaseVector, zero_z?: boolean, tol?: number): boolean
+export declare function winding(a: BaseVector, b: BaseVector, c: BaseVector, zero_z?: boolean, tol?: number): boolean;
 
-export declare function tri_area(a: BaseVector, b: BaseVector, c: BaseVector): number
+export declare function tri_area(a: BaseVector, b: BaseVector, c: BaseVector): number;
 
-export declare function point_in_tri(p: BaseVector, a: BaseVector, b: BaseVector, c: BaseVector): number
+export declare function point_in_tri(p: BaseVector, a: BaseVector, b: BaseVector, c: BaseVector): number;
 
-export declare function normal_tri(a: Vector3, b: Vector3, c: Vector3): Vector3
+export declare function normal_tri(a: Vector3, b: Vector3, c: Vector3): Vector3;
 
-export declare function normal_quad(a: Vector3, b: Vector3, c: Vector3, d: Vector3): Vector3
+export declare function normal_quad(a: Vector3, b: Vector3, c: Vector3, d: Vector3): Vector3;
 
-export declare function dihedral_v3_sqr(a: Vector3, b: Vector3, c: Vector3, d: Vector3): number
+export declare function dihedral_v3_sqr(a: Vector3, b: Vector3, c: Vector3, d: Vector3): number;
 
-export declare function normal_poly(vs: Vector3[]): Vector3
+export declare function normal_poly(vs: Vector3[]): Vector3;
 
 export declare function line_line_isect<VectorType extends BaseVector>(
-    l1: VectorType,
-    l2: VectorType,
-    l3: VectorType,
-    l4: VectorType,
-    test_segment?: boolean
-): VectorType | number
+  l1: VectorType,
+  l2: VectorType,
+  l3: VectorType,
+  l4: VectorType,
+  test_segment?: boolean
+): VectorType | number;
 
-export declare function line_line_cross(a: BaseVector, b: BaseVector, c: BaseVector, d: BaseVector): boolean
+export declare function line_line_cross(a: BaseVector, b: BaseVector, c: BaseVector, d: BaseVector): boolean;
 
-export declare const COLINEAR_ISECT: number
+export declare const COLINEAR_ISECT: number;
 
 export declare function dist_to_line_2d(
-    p: Vector2,
-    v1: Vector2,
-    v2: Vector2,
-    clip?: boolean,
-    closest_co_out: Vector3,
-    t_out: [number]
-): number
+  p: Vector2,
+  v1: Vector2,
+  v2: Vector2,
+  clip?: boolean,
+  closest_co_out?: Vector3,
+  t_out?: [number]
+): number;
