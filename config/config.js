4c964edb96e4072d8e65c2bb9b0955ac5a2a6819
Joe Eagar <joe@circuit.ly>
2025-04-14 20:04:21 +0000

    update config.js

--- config/config.js
+++ config/config.js
@@ -1,20 +1,18 @@
 export let config = {
-  doubleClickTime : 500,
+    doubleClickTime: 500,
 
-  //auto load 1d bspline templates, can hurt startup time
-  autoLoadSplineTemplates : true,
+    //auto load 1d bspline templates, can hurt startup time
+    autoLoadSplineTemplates: true,
 
-  //timeout for press-and-hold (touch) version of double clicking
-  doubleClickHoldTime : 750,
-  DEBUG : {
-
-  }
-};
-
-export function setConfig(obj) {
-  for (let k in obj) {
-    config[k] = obj[k];
-  }
+    //timeout for press-and-hold (touch) version of double clicking
+    doubleClickHoldTime: 750,
+    DEBUG: {}
 }
 
-export default config;
+export function setConfig(obj) {
+    for (let k in obj) {
+        config[k] = obj[k]
+    }
+}
+
+export default config
